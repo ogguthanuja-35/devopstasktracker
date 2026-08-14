cfcb8406fe6852158cc90599d53fab78a64bab4c
ogguthanuja-35 <ogguthanuja@gmail.com>
2026-08-14 02:35:42 +0000

    Initial devops task tracker

---
+++ README.md
@@ -0,0 +1,10 @@
+# DevOps Task Tracker
+
+A simple Python project for practicing Git and DevOps workflows.
+
+## Tasks
+
+- Linux
+- Git
+- Docker
+- Jenkins
---
+++ app.py
@@ -0,0 +1,11 @@
+tasks = [
+    "Learn Linux",
+    "Learn Git",
+    "Learn Docker",
+    "Learn Jenkins"
+]
+
+print("DevOps Task Tracker")
+
+for task in tasks:
+    print("-", task)
